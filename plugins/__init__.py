8c22cb444b71eb450ade5ecae6c21b1f8f10106f
Jaden <166510959+Jadexzc@users.noreply.github.com>
2025-11-03 05:00:29 +0800

    Fix PluginManager import error for robust modular loading.

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,5 +1,6 @@
 """RedCortex plugins package.
-
 This directory contains dynamic security scanning plugins.
 Each plugin should implement a run(response, url) function.
 """
+
+from plugins import PluginManager
